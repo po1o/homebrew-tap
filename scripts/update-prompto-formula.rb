a65064d5c277bc075d4a5070993b2471650e20a2
Polo <po1o@baloo.io>
2026-03-15 17:39:11 +0100

    feat: bootstrap prompto tap

---
+++ scripts/update-prompto-formula.rb
@@ -0,0 +1,179 @@
+#!/usr/bin/env ruby
+# frozen_string_literal: true
+
+require 'json'
+require 'net/http'
+require 'uri'
+
+OWNER = 'po1o'
+REPO = 'prompto'
+HOMEPAGE = "https://github.com/#{OWNER}/#{REPO}"
+LICENSE = 'MIT'
+FORMULA_PATH = File.expand_path('../Formula/prompto.rb', __dir__)
+PROTOC_GEN_GO_VERSION = 'v1.36.11'
+PROTOC_GEN_GO_GRPC_VERSION = 'v1.6.1'
+
+Asset = Struct.new(:url, :sha256, keyword_init: true)
+ReleaseAssets = Struct.new(:version, :darwin_amd64, :darwin_arm64, :linux_amd64, :linux_arm64, keyword_init: true)
+
+
+def fetch_json(url)
+  uri = URI(url)
+  request = Net::HTTP::Get.new(uri)
+  request['Accept'] = 'application/vnd.github+json'
+  request['User-Agent'] = 'po1o-homebrew-formula-updater'
+
+  response = Net::HTTP.start(uri.hostname, uri.port, use_ssl: uri.scheme == 'https') do |http|
+    http.request(request)
+  end
+
+  unless response.is_a?(Net::HTTPSuccess)
+    raise "request failed for #{url}: #{response.code} #{response.message}\n#{response.body}"
+  end
+
+  JSON.parse(response.body)
+end
+
+
+def fetch_text(url)
+  uri = URI(url)
+  request = Net::HTTP::Get.new(uri)
+  request['Accept'] = 'application/octet-stream'
+  request['User-Agent'] = 'po1o-homebrew-formula-updater'
+
+  response = Net::HTTP.start(uri.hostname, uri.port, use_ssl: uri.scheme == 'https') do |http|
+    http.request(request)
+  end
+
+  unless response.is_a?(Net::HTTPSuccess)
+    raise "request failed for #{url}: #{response.code} #{response.message}\n#{response.body}"
+  end
+
+  response.body
+end
+
+
+def parse_checksums(text)
+  text.each_line.each_with_object({}) do |line, checksums|
+    next if line.strip.empty?
+
+    sha256, name = line.strip.split(/\s+/, 2)
+    next if sha256.nil? || name.nil?
+
+    checksums[name] = sha256
+  end
+end
+
+
+def fetch_release_assets(version)
+  release = fetch_json("https://api.github.com/repos/#{OWNER}/#{REPO}/releases/tags/v#{version}")
+  assets = release.fetch('assets')
+  asset_urls = assets.each_with_object({}) { |asset, memo| memo[asset.fetch('name')] = asset.fetch('browser_download_url') }
+  checksums_url = asset_urls.fetch('checksums.txt')
+  checksums = parse_checksums(fetch_text(checksums_url))
+
+  required_assets = {
+    'prompto-darwin-amd64' => :darwin_amd64,
+    'prompto-darwin-arm64' => :darwin_arm64,
+    'prompto-linux-amd64' => :linux_amd64,
+    'prompto-linux-arm64' => :linux_arm64,
+  }
+
+  asset_values = required_assets.each_with_object({}) do |(name, key), memo|
+    memo[key] = Asset.new(
+      url: asset_urls.fetch(name),
+      sha256: checksums.fetch(name),
+    )
+  end
+
+  ReleaseAssets.new(version: release.fetch('tag_name').sub(/^v/, ''), **asset_values)
+end
+
+
+def render_formula(release)
+  version_line = release ? "  version \"#{release.version}\"\n" : ''
+  platform_urls = render_platform_urls(release)
+  platform_urls = "#{platform_urls}\n\n" unless platform_urls.empty?
+
+  <<~RUBY
+    class Prompto < Formula
+      desc "Prompt renderer with streaming daemon support"
+      homepage "#{HOMEPAGE}"
+      license "#{LICENSE}"
+#{version_line}      head "#{HOMEPAGE}.git", branch: "main"
+
+#{platform_urls}      depends_on "go" => :build if build.head?
+      depends_on "protobuf" => :build if build.head?
+
+      def install
+        if build.head?
+          ENV["CGO_ENABLED"] = "0"
+          ENV["GOEXPERIMENT"] = "greenteagc,jsonv2"
+          gobin = buildpath/"bin"
+          ENV["GOBIN"] = gobin
+          ENV.prepend_path "PATH", gobin
+
+          system "go", "install", "google.golang.org/protobuf/cmd/protoc-gen-go@#{PROTOC_GEN_GO_VERSION}"
+          system "go", "install", "google.golang.org/grpc/cmd/protoc-gen-go-grpc@#{PROTOC_GEN_GO_GRPC_VERSION}"
+
+          cd "src" do
+            system "go", "generate", "./..."
+
+            ldflags = %W[
+              -s -w
+              -X github.com/#{OWNER}/#{REPO}/src/build.Version=0.0.0-dev
+            ]
+            system "go", "build", *std_go_args(output: bin/"prompto", ldflags: ldflags), "."
+          end
+          return
+        end
+
+        binary_name = if OS.mac?
+          Hardware::CPU.arm? ? "prompto-darwin-arm64" : "prompto-darwin-amd64"
+        else
+          Hardware::CPU.arm? ? "prompto-linux-arm64" : "prompto-linux-amd64"
+        end
+
+        bin.install binary_name => "prompto"
+      end
+
+      test do
+        output = shell_output("\#{bin}/prompto version").strip
+        assert_match(/\\S+/, output)
+      end
+    end
+  RUBY
+end
+
+
+def render_platform_urls(release)
+  return '' unless release
+
+  <<~RUBY
+    on_macos do
+      if Hardware::CPU.arm?
+        url "#{release.darwin_arm64.url}"
+        sha256 "#{release.darwin_arm64.sha256}"
+      else
+        url "#{release.darwin_amd64.url}"
+        sha256 "#{release.darwin_amd64.sha256}"
+      end
+    end
+
+    on_linux do
+      if Hardware::CPU.arm?
+        url "#{release.linux_arm64.url}"
+        sha256 "#{release.linux_arm64.sha256}"
+      else
+        url "#{release.linux_amd64.url}"
+        sha256 "#{release.linux_amd64.sha256}"
+      end
+    end
+  RUBY
+end
+
+version = ARGV[0]&.strip
+version = nil if version.to_s.empty?
+release = version ? fetch_release_assets(version) : nil
+File.write(FORMULA_PATH, render_formula(release))
+puts "wrote #{FORMULA_PATH}#{release ? " for v#{release.version}" : ' as head-only'}"
